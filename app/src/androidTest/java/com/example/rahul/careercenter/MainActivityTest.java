1242391c261133292a92a5d70f04b10f354fc9c2
Rahul prajapati <raarav717@gmail.com>
2018-03-16 13:11:26 -0300

    Initial commit

---
+++ app/src/androidTest/java/com/example/rahul/careercenter/MainActivityTest.java
@@ -0,0 +1,10 @@
+package com.example.rahul.careercenter;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Rahul on 3/16/2018.
+ */
+public class MainActivityTest {
+
+}
---
+++ app/src/main/java/com/example/rahul/careercenter/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.rahul.careercenter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+@SuppressWarnings("ALL")
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://www.google.com");
+    }
+}
